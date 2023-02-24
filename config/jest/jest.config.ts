3477fa881029c81614dd08938298f05b8ec42d4f
Arsenyev Aleksandr <aar@intelsy.pro>
2023-02-24 18:10:44 +0300

    fix setup test jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,9 +3,19 @@
  * https://jestjs.io/docs/configuration
  */
 
-import path from 'path';
+import path from "path";
 
 export default {
+  // All imported modules in your tests should be mocked automatically
+  // automock: false,
+
+  // Stop running tests after `n` failures
+  // bail: 0,
+
+  // The directory where Jest should store its cached dependency information
+  // cacheDirectory: "C:\\Users\\tim\\AppData\\Local\\Temp\\jest",
+
+  // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
   testEnvironment: 'jsdom',
   coveragePathIgnorePatterns: [
@@ -19,21 +29,19 @@ export default {
     'json',
     'node',
   ],
+  modulePaths: [ '<rootDir>src' ],
   moduleDirectories: [
-    'node_modules',
-  ],
-  modulePaths: [
-    '<rootDir>src',
+    'node_modules'
   ],
   testMatch: [
     // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
+  setupFilesAfterEnv: ['<rootDir>/config/jest/setupTest.ts'],
   rootDir: '../../',
-  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
-    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+    '\\.svg': path.resolve(__dirname, 'JestEmptyComponent.tsx')
   },
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
