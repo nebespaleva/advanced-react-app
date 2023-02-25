d46046a716b5286923c3baf38d29584a04020c2b
Arsenyev Aleksandr <aar@intelsy.pro>
2023-02-25 11:03:33 +0300

    fix setup test jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,6 +3,8 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from "path";
+
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -39,7 +41,7 @@ export default {
   rootDir: '../../',
   moduleNameMapper: {
     '\\.s?css$': 'identity-obj-proxy',
-    '\\.svg$': './svg.js'
+    '\\.svg': path.resolve(__dirname, 'JestEmptyComponent.tsx'),
   },
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
