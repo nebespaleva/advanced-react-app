a694c68ac7ee5f12ffe35346dfaee98ea7347526
Arsenyev Aleksandr <aar@intelsy.pro>
2023-03-03 11:59:09 +0300

    fix AuthModal

--- src/features/AuthByUsername/ui/AuthModal/ui/AuthModal.tsx
+++ src/features/AuthByUsername/ui/AuthModal/ui/AuthModal.tsx
@@ -1,7 +1,5 @@
 import { type FC } from 'react'
-import { classNames } from 'shared/lib'
 import { Modal } from 'shared/ui'
-import styles from './AuthModal.module.scss'
 import { AuthForm } from '../../AuthForm/ui/AuthForm'
 
 interface AuthModalProps {
@@ -12,7 +10,6 @@ interface AuthModalProps {
 export const AuthModal: FC<AuthModalProps> = ({ isOpen, onClose }) => {
   return (
       <Modal
-          className={classNames(styles.authModal, {}, [])}
           isOpen={isOpen}
           onClose={onClose}
           lazy
